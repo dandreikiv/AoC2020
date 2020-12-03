bd289c6c2dce5821422e3709a74d650cd2e3ca4d
Dmytro Andreikiv <dmytro.andreikiv@ah.nl>
2020-12-03 10:09:12 +0100

    Day3 Part1

--- AOC.playground/Contents.swift
+++ AOC.playground/Contents.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-let dp = Day2Part2()
-dp.loadInput()
-
+let dp = Day3Part1()
 let result = dp.solve()
 print(result)
 
--- AOC.playground/Sources/task.swift
+++ AOC.playground/Sources/task.swift
@@ -1,85 +1,47 @@
 import Foundation
 
-public struct Range {
-    let min: Int
-    let max: Int
-}
-
-public struct PassPolicy {
-    let range: Range
-    let char: Character
-    let password: String
-}
-
-public class Day2Part2 {
+public class Day3Part1 {
     private let url = Bundle.main.url(forResource: "input", withExtension: "txt")
-    private var input: [PassPolicy] = []
+    private var input: [Character] = []
+    private var rows: Int = 0
+    private var cols: Int = 0
 
-    public init() {}
-}
-
-public extension Day2Part2 {
+    public init() {
+        loadInput()
+    }
 
     func loadInput() {
         guard let url = url, let str = try? String(contentsOf: url) else {
             fatalError("Couldn't load input")
         }
         let lines = str.split(separator: "\n")
-        input = lines.compactMap { line in
-            let components = line.split(separator: " ").map { s in
-                s.trimmingCharacters(in: .whitespacesAndNewlines)
-            }
-            assert(components.count == 3, "wrong input")
+        cols = lines.first?.count ?? 0
+        rows = lines.count
 
-            guard let range = parseRange(string: components[0]) else {
-                assertionFailure("Couldn't parse range")
-                return nil
-            }
-
-            guard let char = parseChar(string: components[1]) else {
-                assertionFailure("Couldn't parse char")
-                return nil
-            }
-
-            let password = components[2]
-
-            return PassPolicy(range: range, char: char, password: password)
-        }
+        input = lines.flatMap { s in s.map(Character.init) }
     }
+}
 
-    func parseRange(string: String) -> Range? {
-        let r = string.split(separator: "-")
-        assert(r.count == 2, "wrong range format")
-
-        guard let min = Int(r[0]), let max = Int(r[1]) else { return nil }
-        return Range(min: min, max: max)
-    }
-
-    func parseChar(string: String) -> Character? {
-        return string.map(Character.init).first
-    }
+public extension Day3Part1 {
 
     func solve() -> Int {
-        var count = 0
-        for p in input {
-            if validate(password: p) {
-                count += 1
-            }
+        let stepRight = 3
+        let stepDown = 1
+        var result = 0
+        var x = 0
+        var y = 0
+        var pos = 0
+
+        while y < rows && pos < input.count {
+            x = x + stepRight
+            y = y + stepDown
+
+            if x >= cols { x = x % cols }
+
+            pos = x + y * cols
+            if pos >= input.count { break }
+            result += input[pos] == "#" ? 1 : 0
         }
-        return count
-    }
-
-    func validate(password: PassPolicy) -> Bool {
-        let a = password.password.map(Character.init)
-        let c = password.char
-        let range = password.range
-
-        guard range.min <= a.count else { return false }
-        guard range.max <= a.count else { return false }
-
-        let c1 = a[range.min - 1]
-        let c2 = a[range.max - 1]
-
-        return (c1 == c || c2 == c) && (c1 != c2)
+        return result
     }
 }
