931ceb9d28a10311b38cf8ec0fc9c596c9a7f598
Dmytro Andreikiv <dmytro.andreikiv@ah.nl>
2020-12-09 16:36:55 +0100

    Day 9 Part 2

--- AOC.playground/Contents.swift
+++ AOC.playground/Contents.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-let dp = Day9Part1()
+let dp = Day9Part2()
 let result = dp.solve()
 print(result)
--- AOC.playground/Sources/task.swift
+++ AOC.playground/Sources/task.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public class Day9Part1 {
+public class Day9Part2 {
     private let url = Bundle.main.url(forResource: "input", withExtension: "txt")
     private var input: [Int] = []
 
@@ -17,10 +17,39 @@ public class Day9Part1 {
     }
 }
 
-public extension Day9Part1 {
+public extension Day9Part2 {
 
     func solve() -> Int {
-        let length = 25
+        let invalidNumber = foundInvalidNumber(preambule: 25)
+
+        var slices: [[Int]] = []
+        for i in input {
+            slices.append([i])
+        }
+        
+        var resultSlice: [Int] = []
+        var slicesCount = slices.count
+        while slicesCount > 1 {
+            var found = false
+            for i in 1...(slicesCount - 1) {
+                let nextSliseCount = slices[i].count
+                let index = nextSliseCount - 1
+                slices[i - 1].append(slices[i][index])
+                if slices[i - 1].reduce(0, +) == invalidNumber {
+                    resultSlice = slices[i - 1]
+                    found = true
+                    break
+                }
+            }
+            if found { break }
+            slicesCount = slicesCount - 1
+        }
+
+        resultSlice.sort()
+        return resultSlice.min()! + resultSlice.max()!
+    }
+
+    func foundInvalidNumber(preambule length: Int) -> Int {
         var result = 0
 
         for i in length..<input.count {
