ed8a2abb5d0e8ca6cec8ea2a48a01d91980a9e22
Dmytro Andreikiv <dmytro.andreikiv@ah.nl>
2020-12-11 11:55:14 +0100

    Day 10 Part 2

--- AOC.playground/Contents.swift
+++ AOC.playground/Contents.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-let dp = Day10Part1()
+let dp = Day10Part2()
 let result = dp.solve()
 print(result)
--- AOC.playground/Sources/task.swift
+++ AOC.playground/Sources/task.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public class Day10Part1 {
+public class Day10Part2 {
     private let url = Bundle.main.url(forResource: "input", withExtension: "txt")
     private var input: [Int] = []
 
@@ -13,14 +13,15 @@ public class Day10Part1 {
             fatalError("Couldn't load input")
         }
         input = str.split(separator: "\n").map(String.init).compactMap(Int.init)
-        print(input)
+//        print(input)
     }
 }
 
-public extension Day10Part1 {
+public extension Day10Part2 {
 
     func solve() -> Int {
         input.sort()
+
         if let maxJ = input.max() {
             input.append(maxJ + 3)
         }
@@ -35,7 +36,44 @@ public extension Day10Part1 {
                 tmp = input[i]
             }
         }
-        print(stat)
+
+        var pathMem: [Int: Int] = [:]
+        let dict = prepare(array: [0] + input)
+        let count = traverse(dict: dict, start: 0, pathMem: &pathMem)
+        print(count)
+
         return (stat[1] ?? 0) * (stat[3] ?? 0)
+    }
+
+    func prepare(array a: [Int]) -> [Int: [Int]] {
+        print(a)
+        var dict: [Int: [Int]] = [:]
+        for i in 0...a.count - 2 {
+            var j = i + 1
+            var list: [Int] = []
+            dict[a[i]] = list
+            while j < a.count && a[j] - a[i] <= 3  {
+                list.append(a[j])
+                j = j + 1
+            }
+            dict[a[i]] = list
+        }
+        return dict
+    }
+
+    func traverse(dict: [Int: [Int]], start: Int, pathMem: inout [Int: Int]) -> Int {
+        var count = 0
+
+        if let pathCount = pathMem[start] { return pathCount }
+
+        if let a = dict[start] {
+            for key in a {
+                count += traverse(dict: dict, start: key, pathMem: &pathMem)
+            }
+            pathMem[start] = count
+        } else {
+            return 1
+        }
+        return count
     }
 }
