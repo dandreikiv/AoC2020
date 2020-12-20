ced2bbe25c078e82bf7020446485245017ec532a
Dmytro Andreikiv <dmytro.andreikiv@ah.nl>
2020-12-20 21:40:00 +0100

    Day 16 Part 1

--- AOC.playground/Contents.swift
+++ AOC.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let dp = Day15Part1()
+let dp = Day16Part1()
 let result = dp.solve()
 print("result: \(result)")
 
--- AOC.playground/Sources/task.swift
+++ AOC.playground/Sources/task.swift
@@ -1,8 +1,28 @@
 import Foundation
 
-public class Day15Part1 {
+public class Day16Part1 {
+    struct Range {
+        let start: Int
+        let end: Int
+
+        func contains(number: Int) -> Bool {
+            return start <= number && number <= end
+        }
+    }
+
+    struct RuleRange {
+        let start: Range
+        let end: Range
+
+        func contains(number: Int) -> Bool {
+            return start.contains(number: number) || end.contains(number: number)
+        }
+    }
+
     private let url = Bundle.main.url(forResource: "input", withExtension: "txt")
     private var data: [Int] = []
+    private var rules: [String: RuleRange] = [:]
+    private var tickets: [[Int]] = []
 
     public init() {
         loadInput()
@@ -12,38 +32,63 @@ public class Day15Part1 {
         guard let url = url, let str = try? String(contentsOf: url) else {
             fatalError("Couldn't load input")
         }
-        data = str.split(separator: ",").map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }.compactMap(Int.init)
+        let lines = str.split(separator: "\n").map(String.init)
+        var idx = 0
+        var line = lines[idx]
+        // Get rules
+        while line.contains("your ticket:") == false {
+            let parts = line.split(separator: ":")
+            let rule = String(parts[0])
+            let rangeParts = parts[1].replacingOccurrences(of: "or", with: ",").split(separator: ",").map(String.init)
+
+            let rangeOne = rangeFromString(rangeParts[0])
+            let rangeTwo = rangeFromString(rangeParts[1])
+
+            rules[rule] = RuleRange(start: rangeOne, end: rangeTwo)
+            idx = idx + 1
+            line = lines[idx]
+        }
+        // Skip to the "nearby tickets:"
+        while lines[idx].contains("nearby tickets:") == false {
+            idx = idx + 1
+        }
+        idx = idx + 1
+
+        while idx < lines.count {
+            let ticket = lines[idx].split(separator: ",").map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }
+                .compactMap(Int.init)
+            tickets.append(ticket)
+            idx = idx + 1
+        }
+    }
+
+    func rangeFromString(_ string: String) -> Range {
+        let parts = string.split(separator: "-").map { String($0).trimmingCharacters(in: .whitespacesAndNewlines) }
+        let start = Int(parts[0])!
+        let end = Int(parts[1])!
+        return .init(start: start, end: end)
     }
 }
 
-public extension Day15Part1 {
+public extension Day16Part1 {
 
     func solve() -> Int {
-        print(data)
-        var turns: [Int: [Int]] = [:]
-        for (i, d) in data.enumerated() {
-            turns[d] = [i + 1]
-        }
-        var last = data[data.count - 1]
-        var turn = data.count + 1
-        while turn <= 30000000 {
-            if let t = turns[last] {
-                let newNumber = t.count >= 2 ? t[t.count - 1] - t[t.count - 2] : 0
-
-                if let tn = turns[newNumber] {
-                    turns[newNumber] = [tn.last!, turn]
-                } else {
-                    turns[newNumber] = [turn]
+        var nonValidNumbers: [Int] = []
+        for t in tickets {
+            for n in t {
+                if isNumberValid(number: n) == false {
+                    nonValidNumbers.append(n)
                 }
-
-                last = newNumber
-            } else {
-                last = 0
-                turns[last] = [turn]
             }
-            turn = turn + 1
         }
+        return nonValidNumbers.reduce(0, +)
+    }
 
-        return last
+    func isNumberValid(number: Int) -> Bool {
+        var result = false
+        for (_, v) in rules {
+            result = result || v.contains(number: number)
+        }
+        return result
     }
 }
